5877980c3b7fd134c54faeb080e41107e6af3fbd
Hyunwoo Moon <mhw9163@gmail.com>
2020-11-16 20:14:22 -0800

    shopping challenge

---
+++ index.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+'use strict';
+function shoppingCart(){
+  $('#js-shopping-list-form').on('click', function(event){
+    event.preventDefault();
+    let newItem = $('#shopping-list-entry').val();
+    $('ul').append(`<li>
+        <span class="shopping-item">${newItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+  });
+  $('ul').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+  $('ul').on('click', '.shopping-item-delete', function(event){
+    $(this).closest('li').remove();
+  });  
+}
+$(shoppingCart);
